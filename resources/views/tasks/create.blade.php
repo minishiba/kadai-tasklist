f529b68362ca187975ab487be8ca7c97b5beefe4
shiori.hidaka <shiori.a.hidaka@rakuten.com>
2018-06-01 05:01:03 +0000

    add column

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -8,6 +8,10 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
 
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
+
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -7,6 +7,11 @@
     
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
 
+        
+        {!! Form::label('status', 'ステータス:') !!}
+        {!! Form::text('status') !!}
+
+
         {!! Form::label('content', 'タスク:') !!}
         {!! Form::text('content') !!}
 
